9fbeb13585ff66bad05c16c5b449953c267d0651
Vladislav <121075773+Rocket151@users.noreply.github.com>
2023-06-14 18:01:50 +0000

    Update breadcrumbs.tsx

--- src/components/breadcrumbs/breadcrumbs.tsx
+++ src/components/breadcrumbs/breadcrumbs.tsx
@@ -1,15 +1,23 @@
 import { NavLink } from 'react-router-dom';
 import useBreadcrumbs from 'use-react-router-breadcrumbs';
 import { useAppSelector } from '../../hooks';
+import { getProductData } from '../../store/product-data/selectors';
 const routes = [
   { path: '/product/:id', breadcrumb: ProductBreadcrumb},
-  { path: '/', breadcrumb: Cata },
+  { path: '/', breadcrumb: CatalogBreadcrumb },
 ];
 
 function ProductBreadcrumb(): JSX.Element {
-  const cameraName = useAppSelector();
+  const productData = useAppSelector(getProductData);
+
   return (
-    <span>
+    <span className="breadcrumbs__link breadcrumbs__link--active">{productData?.name}</span>
+  );
+}
+
+function CatalogBreadcrumb(): JSX.Element {
+  return (
+    <span>Каталог
       <svg width="5" height="8" aria-hidden="true">
         <use xlinkHref="#icon-arrow-mini"></use>
       </svg>
@@ -17,21 +25,15 @@ function ProductBreadcrumb(): JSX.Element {
   );
 }
 
-function CatalogBreadcrumb(): JSX.Element {
-  return (
-    <svg width="5" height="8" aria-hidden="true">
-      <use xlinkHref="#icon-arrow-mini"></use>
-    </svg>
-  );
-}
-
 export default function BreadCrumbs(): JSX.Element {
   const breadcrumbs = useBreadcrumbs(routes);
+
   return(
     <div className="breadcrumbs">
       <div className="container">
         <ul className="breadcrumbs__list">
           {breadcrumbs.map(({ match, breadcrumb }) => (
+            match.pathname !== '/product' &&
             <li className="breadcrumbs__item" key={match.pathname}>
               <NavLink className="breadcrumbs__link" to={match.pathname}>
                 {breadcrumb}
