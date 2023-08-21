0bf474e3672120b2ad0de9452e8e6865a8f011b1
Vladislav <121075773+Rocket151@users.noreply.github.com>
2023-08-21 04:57:26 +0000

    Update 4 files

--- src/store/basket-data/basket-data.ts
+++ src/store/basket-data/basket-data.ts
@@ -2,6 +2,7 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { SlicesNames } from '../../const';
 import { BasketDataState } from '../../types/state';
 import { BasketCamerasData } from '../../types/basket-cameras-data';
+import { CamerasData } from '../../types/cameras-data';
 
 export const initialBasketDataState: BasketDataState = {
   basketCamerasData: [],
@@ -11,7 +12,7 @@ export const basketData = createSlice({
   name: SlicesNames.BasketData,
   initialState: initialBasketDataState,
   reducers: {
-    addToBasket: (state, action: PayloadAction<BasketCamerasData>) => {
+    addToBasket: (state, action: PayloadAction<CamerasData>) => {
       if(state.basketCamerasData.length) {
         if(state.basketCamerasData.every((basketCamera) => basketCamera.id === action.payload.id)) {
           state.basketCamerasData.map((basketCameraData) => {
@@ -24,9 +25,31 @@ export const basketData = createSlice({
             return basketCameraData;
           });
         } else {
-          state.basketCamerasData.push(action.payload);
+          state.basketCamerasData.push({
+            ...action.payload,
+            basketItemCount: 1,
+          });
         }
+      } else {
+        state.basketCamerasData.push({
+          ...action.payload,
+          basketItemCount: 1,
+        });
       }
     },
-  },
+
+    changeBasketItemCount: (state, action: PayloadAction<BasketCamerasData>) => {
+      state.basketCamerasData.map((basketCameraData) => {
+        if(basketCameraData.id === action.payload.id) {
+          return {
+            ...basketCameraData,
+            basketItemCount: action.payload.basketItemCount,
+          };
+        }
+        return basketCameraData;
+      });
+    },
+  }
 });
+
+export const {changeBasketItemCount, addToBasket} = basketData.actions;
