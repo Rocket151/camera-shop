29ac415b0e52094fab368d3ba556f503983bf2f2
Vladislav <121075773+Rocket151@users.noreply.github.com>
2023-09-08 06:16:36 +0000

    Рефакторит header.tsx

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -20,8 +20,6 @@ const initialState = {
   name: '',
 };
 
-const inpArr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,25,24,42,42,2,42,42,24,2,4];
-
 export default function Header(): JSX.Element {
   const { markRef } = useKeyboardNavigator({
     eventCallback: (evt) => evt.preventDefault()
@@ -32,7 +30,7 @@ export default function Header(): JSX.Element {
   const [inputValue, setInputValue] = useState('');
   const [highlightBlockIndex, setHighlightBockIndex] = useState(-1);
   const [data, setData] = useState<SearchItemState[] | never[]>([initialState]);
-  const refs: MutableRefObject<RefObject<HTMLLIElement>[]> = useRef(inpArr.map(() => React.createRef()));
+  const refs: MutableRefObject<RefObject<HTMLLIElement>[]> = useRef(camerasData.map(() => React.createRef()));
   const inpRef: LegacyRef<HTMLInputElement> | undefined = useRef(null);
   const closeBtnRef: LegacyRef<HTMLButtonElement> | undefined = useRef(null);
 
